8367c7c63065cac2d6502f9298d652476c3ab965
chathuraed <chathura.dev93@gmail.com>
2023-11-15 01:49:20 +0530

    error handling

--- src/components/error-message/index.tsx
+++ src/components/error-message/index.tsx
@@ -1,55 +1,84 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
 import {selectError} from '../../store/reducer/app-slice';
+import {Colors} from '../../resources';
 
 const ErrorMessage = props => {
-  const {containerStyle, message} = props;
+  const {message} = props;
 
-  const [errorTitle, setErrorTitle] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(true);
+  const [errorTitle, setErrorTitle] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [visible, setVisible] = useState(false);
 
   const error = useSelector(selectError);
 
   useEffect(() => {
     setErrorTitle(error?.title);
     setErrorMessage(error?.message);
-  });
+    setVisible(true);
+
+    // Auto-hide after 3 seconds
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 3000);
+
+    // Clear the timer on component unmount or when the error changes
+    return () => clearTimeout(timer);
+  }, [error]);
+
+  const handlePress = () => {
+    // Hide the error message when touched
+    setVisible(false);
+  };
 
   return (
     <>
-      {errorMessage ? (
-        <View
-          style={[
-            {
-              backgroundColor: 'red',
-              padding: 10,
-              flexDirection: 'row',
-            },
-            containerStyle,
-          ]}>
-          <View style={{flex: 1, marginLeft: 10}}>
-            {errorTitle && errorTitle.length > 0 ? (
-              <View
-                style={{
-                  backgroundColor: 'yellow',
-                }}>
-                <Text style={{color: 'white', fontWeight: 'bold'}}>
-                  {errorTitle}
+      {visible && errorMessage ? (
+        <TouchableOpacity onPress={handlePress}>
+          <View style={styles.container}>
+            <View style={styles.innerContainer}>
+              {errorTitle && errorTitle.length > 0 ? (
+                <View style={styles.titleContainer}>
+                  <Text style={styles.titleText}>{errorTitle}</Text>
+                </View>
+              ) : null}
+
+              <View>
+                <Text style={styles.messageText}>
+                  {errorMessage ? errorMessage : message}
                 </Text>
               </View>
-            ) : null}
-
-            <View>
-              <Text style={{color: 'white'}}>
-                {errorMessage ? errorMessage : message}
-              </Text>
             </View>
           </View>
-        </View>
+        </TouchableOpacity>
       ) : null}
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: Colors.error,
+    padding: 16,
+    flexDirection: 'row',
+  },
+  innerContainer: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  titleContainer: {
+    backgroundColor: 'yellow',
+  },
+  titleText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  messageText: {
+    color: 'white',
+  },
+});
 
 export default ErrorMessage;
---
+++ src/resources/colors.ts
@@ -0,0 +1,16 @@
+// colors.ts
+
+const Colors = {
+  white: '#FFFFFF',
+  green: '#8BC83F',
+  textMain: '#252B5C',
+  textSecondary: '#1F4C6B',
+  backgroundGray: '#ECEDF3',
+  textGray: '#A1A5C1',
+  buttonContainerGray: '#F5F4F8',
+  textLightGray: '#53587A',
+  textLinkBlue: '#1F4C6B',
+  error: '#FD5F4A',
+};
+
+export {Colors};
